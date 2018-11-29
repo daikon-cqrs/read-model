9288f2a82eab0471b2a46371139c72e52a134280
Hasham Ahmad <mr.hash@gmail.com>
2018-11-29 16:22:22 +0000

    Additional native interface to QueryInterface

--- src/Query/QueryInterface.php
+++ src/Query/QueryInterface.php
@@ -3,8 +3,9 @@
 namespace Daikon\ReadModel\Query;
 
 use Daikon\Interop\ToNativeInterface;
+use Daikon\Interop\FromNativeInterface;
 
-interface QueryInterface extends ToNativeInterface
+interface QueryInterface extends FromNativeInterface, ToNativeInterface
 {
 
 }
