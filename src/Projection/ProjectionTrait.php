972750123960859f234b968fc310b0625b12984b
Hasham Ahmad <mr.hash@gmail.com>
2018-11-30 14:11:05 +0000

    Code cleanup

--- src/Projection/ProjectionTrait.php
+++ src/Projection/ProjectionTrait.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the daikon-cqrs/read-model project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace Daikon\ReadModel\Projection;
 
@@ -9,6 +17,7 @@ trait ProjectionTrait
 {
     private $state;
 
+    /** @param array $state */
     public static function fromNative($state): ProjectionInterface
     {
         return new static($state);
